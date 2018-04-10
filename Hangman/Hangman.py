79a7b2f3ddec0573a172a8a2893fee0925887bd1
wiilz <rayleignze@gmail.com>
2018-04-10 23:43:43 +0800

    Fix the bug

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -2,7 +2,7 @@
 from PyQt4 import QtCore, QtGui, uic
 import random
 
-form_class = uic.loadUiType("hangman.ui")
+form_class = uic.loadUiType("hangman.ui")[0]
 
 #查找字母
 def find_letters(letter, a_string):
@@ -45,6 +45,7 @@ class MyWidget(QtGui.QMainWindow, form_class):
         self.gallows = [self.line1, self.line2, self.line3, self.line4]
         self.pieces_shown = 0
         self.currentword = ""
+        
         f = open("words.txt",'r')  #得到词汇表
         self.lines = f.readlines()
         f.close()
@@ -84,21 +85,21 @@ class MyWidget(QtGui.QMainWindow, form_class):
                     self.wrong()
             self.guessBox.setText("")
 
-        def win(self):
-            QtGui.QMessageBox.information(self,"Hangman","You win!")
-            self.new_game()
+    def win(self):
+        QtGui.QMessageBox.information(self,"Hangman","You win!")
+        self.new_game()
 
-        def wrong(self):
-            self.pieces_shown += 1
-            for i in range(self.pieces_shown):
-                self.pieces[i].setHidden(False)
-            if self.pieces_shown == len(self.pieces):
-                message = "You lose. The word was " + self.currentword
-                QtGui.QMessageBox.warning(self,"Hangman",message)
-                self.new_game()
+    def wrong(self):
+        self.pieces_shown += 1
+        for i in range(self.pieces_shown):
+            self.pieces[i].setHidden(False)
+        if self.pieces_shown == len(self.pieces):
+            message = "You lose. The word was " + self.currentword
+            QtGui.QMessageBox.warning(self,"Hangman",message)
+            self.new_game()
                 
-        def menuExit_selected(self):
-            self.close()
+    def menuExit_selected(self):
+        self.close()
 app = QtGui.QApplication(sys.argv)
 myapp = MyWidget(None)
 myapp.show()
